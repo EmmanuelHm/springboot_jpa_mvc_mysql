5a217ad6f9b3886e96db8d9b6cce5adefa9b27e8
Em Hermaq <66495118+EmmanuelHm@users.noreply.github.com>
2022-09-13 16:26:43 -0500

    commit
    CRUD con paginación implementada para el registro de Clientes

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/dao/IClienteDao.java
@@ -1,16 +1,21 @@
 package com.bolsadeideas.springboot.app.models.dao;
 
-import org.springframework.data.repository.CrudRepository;
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 //import java.util.List;
 
 import com.bolsadeideas.springboot.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long>{
+//public interface IClienteDao extends CrudRepository<Cliente, Long>{
 	
 	/*public List<Cliente> findAll();
 	public void save(Cliente cliente);
 	public Cliente findOne(Long id);
 	public void delete(Long id);*/
 
+//}
+
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long>{
+	
 }
--- spring-boot-data-jpa/src/main/resources/import.sql
+++ spring-boot-data-jpa/src/main/resources/import.sql
@@ -1,3 +1,20 @@
 /*Populate tables*/
 INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Emmanuel', 'Hernandez', 'emmanuel@mail.com', '2022-09-11');
-INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('John', 'Doe', 'john@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ivan', 'Hernandez', 'ivan@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Orlando', 'Hernandez', 'orlando@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Angel', 'Estrada', 'angel@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Sofia', 'Carrillo', 'sofia@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Anna', 'Sarazua', 'anna@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Miguel', 'Jimenez', 'miguel@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Francisco', 'Rosado', 'francisco@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Maricarmen', 'Velazquez', 'maricarmen@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alexis', 'Sanchez', 'alexis@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Alfonso', 'Perez', 'alfonso@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Carolina', 'Navarrete', 'carolina@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Joshua', 'Escobar', 'joshua@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ximena', 'Zepeda', 'ximena@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Eric', 'Escamilla', 'eric@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Brenda', 'Soltero', 'brenda@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Fanny', 'Quintero', 'fanny@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('ELizabeth', 'Juarez', 'elizabeth@mail.com', '2022-09-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ricardo', 'Espejel', 'ricardo@mail.com', '2022-09-11');
